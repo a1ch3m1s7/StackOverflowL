7483dfe891d07228222b16c73813ebe01f3c5b9d
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2019-02-08 21:16:38 +0000

    First heroku app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.api.v1.views.party_views import p_v1 as v1
 from app.api.v1.views.office_views import office as v2
 
+
 def politico_app():
     app = Flask(__name__)
     app.register_blueprint(v1, url_prefix="/api/v1")
